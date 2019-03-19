95d6d08e6d8eb1583292fd0086f00f68407901ca
Magdalena Karpacka <karpackamagdalena@gmail.com>
2019-03-19 19:29:41 +0100

    pierwszy probny test, konieczne poprawki

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,12 +1,19 @@
 package edu.iis.mto.bsearch;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
+//    klasy rownowaznosci
 
     @Test
-    void search() {
+    void ifNumberOfInputArgumentsEqualsOneReturnPosition() {
+        int[] seq = {3};
+        int itemToFind = 3;
+        SearchResult searchResult = BinarySearch.search(itemToFind, seq);
+        Assert.assertTrue(searchResult.isFound());
+        Assert.assertEquals(0, searchResult.getPosition());
     }
 }
