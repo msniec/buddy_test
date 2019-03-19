23ecaae8e1b69e966433cbb87e2640ed8abc03a4
Magdalena Karpacka <karpackamagdalena@gmail.com>
2019-03-19 19:06:49 +0100

    stworzenie BinarySearchTest

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.bsearch;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTest {
+
+    @Test
+    void search() {
+    }
+}
