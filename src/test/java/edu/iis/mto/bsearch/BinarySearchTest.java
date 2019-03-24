5baade32b13e3aaa1fff300861a036b43661ab6d
mkarpacka <209348@edu.p.lodz.pl>
2019-03-24 14:41:09 +0100

    testy_v1 ifItemNotExistsInSequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,11 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinarySearchTest {
 
     @Test
-    void ifItemExistReturnPosition() {
+    void ifItemExistsInSequence() {
         int[] seq = {3};
         int itemToFind = 3;
         SearchResult searchResult = BinarySearch.search(itemToFind, seq);
         Assert.assertTrue(searchResult.isFound());
         Assert.assertEquals(0, searchResult.getPosition());
+    }
+
+    @Test
+    void ifItemNotExistsInSequence() {
+        int[] seq = {3};
+        int itemToFind = 4;
+        SearchResult searchResult = BinarySearch.search(itemToFind, seq);
+        Assert.assertFalse(searchResult.isFound());
+        Assert.assertEquals(-1, searchResult.getPosition());
     }
 }
